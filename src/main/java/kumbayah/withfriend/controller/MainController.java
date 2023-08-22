974ff28211716e8c7944d186b94240a8677ccc97
jiggy97 <dkswnsdud159@naver.com>
2023-08-22 21:01:39 +0900

    kakao login 구현중 ..

---
+++ src/main/java/kumbayah/withfriend/controller/MainController.java
@@ -0,0 +1,23 @@
+package kumbayah.withfriend.controller;
+
+import kumbayah.withfriend.service.KakaoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+    private final KakaoService kakaoService;
+
+    MainController(KakaoService kakaoService) {
+        this.kakaoService = kakaoService;
+    }
+
+    @GetMapping("/")
+    public String login(Model model) {
+        model.addAttribute("kakaoUrl", kakaoService.getKakaoLogin());
+        return "index";
+    }
+}
--- src/main/java/kumbayah/withfriend/controller/user/KakaoLoginController.java
+++ src/main/java/kumbayah/withfriend/controller/user/KakaoLoginController.java
@@ -1,14 +1,14 @@
 package kumbayah.withfriend.controller.user;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/user")
 public class KakaoLoginController {
-    @GetMapping("/user/kakao/callback")
-    public String KakaoLogin(String code) {
-        // code는 카카오 서버로부터 받은 인가 코드
-
+    @RequestMapping("/kakao/login")
+    public String login(@RequestParam(value = "code") String code) {
+        return null;
     }
-
 }
