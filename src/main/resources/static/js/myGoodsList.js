ebff8817c02a250785dd128c0828da601ad874cd
jiggy97 <dkswnsdud159@naver.com>
2023-10-06 23:06:01 +0900

    feat: frontend file structuring

--- src/main/resources/static/js/myGoodsList.js
+++ src/main/resources/static/js/myGoodsList.js
@@ -1,4 +1,4 @@
 const detailAndUpdate = (button) => {
-  const id = button.getAttribute('data-goods-id');
-  location.href = "/trust/goods/" + id;
+    const id = button.getAttribute('data-goods-id');
+    location.href = "/trust/goods/" + id;
 }
