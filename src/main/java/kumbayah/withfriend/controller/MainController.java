d9f76b01c1c93aab865073b002c28abdaee6f82a
jiggy97 <dkswnsdud159@naver.com>
2023-10-21 23:58:54 +0900

    Autowired 추가

--- src/main/java/kumbayah/withfriend/controller/MainController.java
+++ src/main/java/kumbayah/withfriend/controller/MainController.java
@@ -5,6 +5,7 @@ import kumbayah.withfriend.dto.kakao.KakaoDTO;
 import kumbayah.withfriend.dto.user.UserDTO;
 import kumbayah.withfriend.service.kakao.KakaoService;
 import kumbayah.withfriend.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,13 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/main")
 public class MainController {
-    private final KakaoService kakaoService;
-    private final UserService userService;
-
-    MainController(KakaoService kakaoService, UserService userService) {
-        this.kakaoService = kakaoService;
-        this.userService = userService;
-    }
+    @Autowired
+    private KakaoService kakaoService;
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/")
     public String login(Model model) {
