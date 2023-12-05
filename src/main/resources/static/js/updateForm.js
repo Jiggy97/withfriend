c6b9cd50da5b25da2584017e09c4809fab18a160
안준영 <dkswnsdud159@naver.com>
2023-12-05 17:10:55 +0900

    refactoring: Spring Boot에서 RESTful API 설계 중,,

--- src/main/resources/static/js/updateForm.js
+++ src/main/resources/static/js/updateForm.js
@@ -1,32 +1,22 @@
 function updateGoods() {
     // 폼 데이터 수집
-    const formData = {
-        id: document.getElementById('id').value,
-        name: document.getElementById('name').value,
-        price: document.getElementById('price').value,
-        description: document.getElementById('description').value,
-        stock: document.getElementById('stock').value,
-    };
+    const formData = new FormData(document.querySelector('form'));
 
-    // PATCH 메서드로 API 호출
-    fetch('/trust/goods/', {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Success:', data);
-        // 성공적으로 처리된 경우 추가 로직을 여기에 작성
-    })
-    .catch(error => {
-        console.error('Error:', error);
+    // 폼 데이터를 객체로 변환
+    const formDataObject = {};
+    formData.forEach((value, key) => {
+        formDataObject[key] = value;
     });
+    console.log(formDataObject);
+
+    // Axios를 사용하여 API 호출
+    axios.patch('/goods/', formDataObject)
+        .then(response => {
+            console.log('Success:', response.data);
+            // 성공적인 응답 처리
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // 오류 처리
+        });
 }
