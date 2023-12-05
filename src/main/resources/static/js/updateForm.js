575b363729f4cda29403eda9098b76826703cd4d
안준영 <dkswnsdud159@naver.com>
2023-12-05 17:11:23 +0900

    refactoring: Spring Boot에서 RESTful API 설계 중,,

--- src/main/resources/static/js/updateForm.js
+++ src/main/resources/static/js/updateForm.js
@@ -1,22 +1,17 @@
 function updateGoods() {
-    // 폼 데이터 수집
     const formData = new FormData(document.querySelector('form'));
 
-    // 폼 데이터를 객체로 변환
     const formDataObject = {};
     formData.forEach((value, key) => {
         formDataObject[key] = value;
     });
     console.log(formDataObject);
 
-    // Axios를 사용하여 API 호출
     axios.patch('/goods/', formDataObject)
         .then(response => {
             console.log('Success:', response.data);
-            // 성공적인 응답 처리
         })
         .catch(error => {
             console.error('Error:', error);
-            // 오류 처리
         });
 }
